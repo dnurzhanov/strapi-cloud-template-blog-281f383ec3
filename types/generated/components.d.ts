dde51c702e00194be5d088d116894fea2f20a94d
Daulet Nurzhanov <nurzhanovdev@gmail.com>
2024-07-24 16:44:18 +0500

    poc v2

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,15 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface WidgetCarousel extends Schema.Component {
+  collectionName: 'components_widget_carousels';
+  info: {
+    displayName: 'Carousel';
+  };
+  attributes: {
+    code: Attribute.String;
+  };
+}
+
 export interface WidgetPredictionCard extends Schema.Component {
   collectionName: 'components_widget_prediction_cards';
   info: {
@@ -13,6 +23,7 @@ export interface WidgetPredictionCard extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'widget.carousel': WidgetCarousel;
       'widget.prediction-card': WidgetPredictionCard;
     }
   }
